638cc4847dc01fc4b407e9d5fdebfd66ce889747
MarceloContrerasGonzalez <marcelocg2002@gmail.com>
2024-10-24 20:30:35 -0300

    Creacion y configuracion de las vistas y los templates de polls

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 # siempre que se cree una vista se debe importar en este archivo para que se pueda acceder a ella
 # a la vez cada vez que se agregue un archivo urls.py en una app se debe importar en el archivo urls.py del proyecto (mysite/urls.py)
+app_name = 'polls' #se especifica el nombre de la app para que se pueda acceder a ella desde la raiz del proyecto
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:pregunta_id>/', views.detail, name='detail'), #se especifica views.detail para que se pueda acceder a la vista detail tiene que tener el mismo nombre que la vista
+    # ex: /polls/5/results/
+    path('<int:pregunta_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:pregunta_id>/vote/', views.vote, name='vote'),
+    
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+#from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import pregunta as Pregunta ##marco Pregunta con mayuscula para que sea mas facil de identificar
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola, mundo. Estas en el index de las encuestas.")
+    #se obtienen las ultimas 5 preguntas y se las ordena por fecha de publicacion
+    latest_question_list = Pregunta.objects.order_by('-fecha_publicacion')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+#se crea una vista para mostrar el detalle de una pregunta
+def detail(request, pregunta_id):
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, "polls/detail.html", {'pregunta': pregunta})
+        
+
+#se crea una vista para mostrar los resultados de una pregunta
+def results(request, pregunta_id):
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, "polls/results.html", {'pregunta': pregunta})
+
+#se crea una vista para votar en una pregunta
+def vote(request, pregunta_id):
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, "polls/vote.html", {'pregunta': pregunta})
